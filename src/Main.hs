a2bd5943d03d87124ff32cc5e15d73c3654a4557
Dmitry Stepin <distepin@gmail.com>
2016-12-20 02:01:12 +0300

    добавил магию мемасов и еще комментариев

--- src/Main.hs
+++ src/Main.hs
@@ -40,17 +40,25 @@ main = do
   -- токен бота
   let token = Token "bot319624564:AAE_fb6q_eTI942c4K7wpC4kNReC28939RI"
   -- основная функция
-  -- нафиг тут все эти нафинги?
+  -- нафинги нужны для getUpdates
+  -- оператор $$ передает данные из updates в processUpdate, вообще офигеть
   updates token Nothing Nothing Nothing manager $$ DC.mapM_ (processUpdate token manager)
 
---что происходит в этой функции?
+-- приём входящих сообщений
+-- offset - идентификатор входящих сообщений (с каждым сообщением увеличивается на 1)
+-- limit - макс. кол-во сообщений принятых за раз (= 100 по дефолту)
+-- timeout - задержка в секундах для работы getUpdates (= 0 по дефолту)
+-- пишут, что надо поставить таймаут больше 0, не знаю зачем, вроде и так работает
+-- https://core.telegram.org/bots/API - тут вся инфа, в общем
 updates :: (MonadIO m) => Token -> Maybe Int -> Maybe Int -> Maybe Int -> Manager -> Source m Update
 updates token offset limit timeout manager = do
   offsetHolder <- liftIO $ atomically $ newTVar offset
+  -- repeatM запускает getUpdatesBatch несколько раз, пока есть входящие сообщения
   DC.repeatM (getUpdatesBatch offsetHolder) =$= DC.concatMap id
     where
       getUpdatesBatch offsetHolder = do
         currentOffset <- liftIO $ atomically $ readTVar offsetHolder
+        -- ответ сервера телеграма (список новых сообщений)
         resp <- liftIO $ getUpdates token currentOffset limit timeout manager
         case resp of
           Left e -> error $ show e
@@ -64,7 +72,7 @@ updates token offset limit timeout manager = do
                 liftIO $ atomically $ writeTVar offsetHolder newOffset
                 return batch
 
--- какая-то дичь с войдами
+-- обработка входящего сообщения и какая-то дичь с войдами
 processUpdate :: (MonadThrow m, MonadIO m) => Token -> Manager -> Update -> m ()
 processUpdate token manager update = void $ runMaybeT $ do
     -- msg - сообщение c дополнительной информацией, пришедшее от пользователя телеграма
@@ -85,27 +93,30 @@ processUpdate token manager update = void $ runMaybeT $ do
             chatId = pack $ show $ chat_id $ chat msg
             request = sendMessageRequest chatId reply
           void $ liftIO $ sendMessage token request manager    
+
+        --функция для отправки картинки в чат
+        sendImg msg img = do
+          let
+            chatId = pack $ show $ chat_id $ chat msg
+            request = sendPhotoRequest chatId img 
+          void $ liftIO $ sendPhoto token request manager
   
         -- функция возвращает функцию, если есть соответствующая ей команда
         tryProcessCommand msg txt = isJustT $ do
           (cmd, handler) <- hoistMaybe $ L.find (checkCommand txt) commands
-          let
-            n = T.length cmd
-            args = T.drop (n + 2) txt
-          handler msg args
+          handler msg (T.drop (T.length cmd + 2) txt)
         
         -- функция проверяет - есть ли такая команда (cmd) в списке команд (commands)
-        checkCommand txt (cmd, _) =
-          let n = T.length cmd
-          in T.take (n + 2) txt == "/" <> cmd
+        checkCommand txt (cmd, _) = T.take (T.length cmd + 2) txt == "/" <> cmd
 
         -- список команд, которые принимает бот
         commands = [ ("start", startCmd), ("help", helpCmd), ("hoogle", hoogleCmd) ]
             
-        -- старт и хелп - должны быть по стандарту у телеграмовского бота
-        -- надо справку какую-нибудь выводить
-        startCmd msg args = do sendReply msg "старт"
-        helpCmd msg args = do sendReply msg "хелп"
+        -- старт - магия мемасов
+        startCmd msg args = do sendImg msg "https://ipic.su/img/img7/fs/vzhuh.1482187468.jpg"
+        -- хелп - надо справку по использованию бота написать
+        helpCmd msg args = do sendReply msg "здесь должна быть справка"
         -- команда, которая парсит хугл и возвращает справку по функциям
         -- наше основное заднание, ну вы поняли
+        -- делайте давайте
         hoogleCmd msg args = do sendReply msg "хугл"
