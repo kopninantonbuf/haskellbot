0dcd052aff5b2cbb5478a8bf5c6f2a1a595db269
Dmitry Stepin <distepin@gmail.com>
2016-12-20 19:31:59 +0300

    обработка некорретных запросовв /hoogle

--- src/Main.hs
+++ src/Main.hs
@@ -125,8 +125,11 @@ processUpdate token manager update = void $ runMaybeT $ do
         -- команда, которая парсит хугл и возвращает справку по функциям
         hoogleCmd msg args = do
           HoogleResponse { results = res } <- hoogle args 0 5
-          when (T.length args > 0 && length res > 0) $ do
-            sendReply msg $ formatHoogleResults res
+          when (T.length args > 0) $ do
+            case (length res) of 
+                0 -> sendReply msg $ "Not found: " <> args    
+                _ -> sendReply msg $ formatHoogleResults res
+                      
 
         formatHoogleResults =
           L.foldl1' (\x y -> x <> "  \n" <> y) . map (("-- " <>) . formatHoogleResult)
