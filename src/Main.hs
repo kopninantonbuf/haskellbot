425a94a7867e1b63316d1fd550ff73af0eefa1d4
Dmitry Stepin <distepin@gmail.com>
2016-12-20 18:53:33 +0300

    переписал текст справки с использованием монадических функций(круто звучит)

--- src/Main.hs
+++ src/Main.hs
@@ -44,8 +44,8 @@ main = do
   let token = Token "bot319624564:AAE_fb6q_eTI942c4K7wpC4kNReC28939RI"
   -- основная функция
   -- нафинги нужны для getUpdates
-  -- оператор $$ передает данные из updates в processUpdate, вообще офигеть
-  updates token Nothing Nothing Nothing manager $$ DC.mapM_ (processUpdate token manager)
+  -- оператор $$ передает данные из botUpdates в processUpdate, вообще офигеть
+  botUpdates token Nothing Nothing Nothing manager $$ DC.mapM_ (processUpdate token manager)
 
 -- приём входящих сообщений
 -- offset - идентификатор входящих сообщений (с каждым сообщением увеличивается на 1)
@@ -53,14 +53,14 @@ main = do
 -- timeout - задержка в секундах для работы getUpdates (= 0 по дефолту)
 -- пишут, что надо поставить таймаут больше 0, не знаю зачем, вроде и так работает
 -- https://core.telegram.org/bots/API - тут вся инфа, в общем
-updates :: (MonadIO m) => Token -> Maybe Int -> Maybe Int -> Maybe Int -> Manager -> Source m Update
-updates token offset limit timeout manager = do
-  offsetHolder <- liftIO $ atomically $ newTVar offset
+botUpdates :: (MonadIO m) => Token -> Maybe Int -> Maybe Int -> Maybe Int -> Manager -> Source m Update
+botUpdates token offset limit timeout manager = do
+  oldOffset <- liftIO $ atomically $ newTVar offset
   -- repeatM запускает getUpdatesBatch несколько раз, пока есть входящие сообщения
-  DC.repeatM (getUpdatesBatch offsetHolder) =$= DC.concatMap id
+  DC.repeatM (getUpdatesBatch oldOffset) =$= DC.concatMap id
     where
-      getUpdatesBatch offsetHolder = do
-        currentOffset <- liftIO $ atomically $ readTVar offsetHolder
+      getUpdatesBatch oldOffset = do
+        currentOffset <- liftIO $ atomically $ readTVar oldOffset
         -- ответ сервера телеграма (список новых сообщений)
         resp <- liftIO $ getUpdates token currentOffset limit timeout manager
         case resp of
@@ -72,7 +72,7 @@ updates token offset limit timeout manager = do
                 let
                   maxUpdateId = maximum $ map update_id batch
                   newOffset = Just (maxUpdateId + 1)
-                liftIO $ atomically $ writeTVar offsetHolder newOffset
+                liftIO $ atomically $ writeTVar oldOffset newOffset
                 return batch
 
 -- обработка входящего сообщения и какая-то дичь с войдами
@@ -106,24 +106,30 @@ processUpdate token manager update = void $ runMaybeT $ do
 
         -- функция возвращает функцию, если есть соответствующая ей команда
         tryProcessCommand msg txt = isJustT $ do
-          (cmd, handler) <- hoistMaybe $ L.find (checkCommand txt) commands
+          (cmd, handler) <- hoistMaybe $ L.find (findCmd txt) commands
           handler msg (T.drop (T.length cmd + 2) txt)
 
         -- функция проверяет - есть ли такая команда (cmd) в списке команд (commands)
-        checkCommand txt (cmd, _) = T.take (T.length cmd + 1) txt == "/" <> cmd
+        findCmd txt (cmd, _) = T.take (T.length cmd + 1) txt == "/" <> cmd
 
         -- список команд, которые принимает бот
         commands = [ ("start", startCmd), ("help", helpCmd), ("hoogle", hoogleCmd) ]
 
         -- старт - магия мемасов
         startCmd msg args = do sendImg msg "https://ipic.su/img/img7/fs/vzhuh.1482187468.jpg"
-        -- хелп - надо справку по использованию бота написать
-        helpCmd msg args = do sendReply msg "Для того, чтобы воспользоваться ботом необходимо ввести команду hoogle с параметрами (либо названием функции, для которой требуется получить описание, либо её сигнатуру)"
+        -- хелп - справка
+        helpCmd msg args = 
+          do sendReply msg $ "Для того, чтобы воспользоваться ботом необходимо ввести " <> 
+                            "команду hoogle с параметрами (либо названием функции, " <>
+                            "для которой требуется получить описание, либо её сигнатуру)"
         -- команда, которая парсит хугл и возвращает справку по функциям
         hoogleCmd msg args = do
           HoogleResponse { results = res } <- hoogle args 0 5
-          when (T.length args > 0 && length res > 0) $ do sendReply msg $ formatHoogleResults res
+          when (T.length args > 0 && length res > 0) $ do 
+            sendReply msg $ formatHoogleResults res
  
-        formatHoogleResults = L.foldl1' (\x y -> x <> "  \n" <> y) . map (("1. " <>) . formatHoogleResult)
+        formatHoogleResults = 
+          L.foldl1' (\x y -> x <> "  \n" <> y) . map (("1. " <>) . formatHoogleResult)
 
-        formatHoogleResult res = self res <> "  \n" <> docs res <> "  \n" <> Hoogle.location res
+        formatHoogleResult res = 
+          self res <> "  \n" <> docs res <> "  \n" <> Hoogle.location res
